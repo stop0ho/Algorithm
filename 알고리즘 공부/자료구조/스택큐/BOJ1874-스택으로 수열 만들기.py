0b8848633b24247074c840be0fff7a238c590afc
죠 <68852637+stop0ho@users.noreply.github.com>
2022-10-05 23:08:55 +0900

    Add stack solution

--- 알고리즘 공부/자료구조/스택큐/BOJ1874-스택으로 수열 만들기.py
+++ 알고리즘 공부/자료구조/스택큐/BOJ1874-스택으로 수열 만들기.py
@@ -1,10 +1,29 @@
-from collections import deque
+n = int(input())    # 수열의 개수
+a = []              # 수열 입력값 저장
+s = []              # stack 초기화
+res = ""            # 정답값
+num = 1
+p = True
 
-n = int(input())
-C = deque(range(1, n+1))
+for _ in range(n):
+    a.append(int(input()))
 
-while len(C) > 1:
-    C.popleft()
-    C.append(C.popleft())
-    
-print(C[0])
+for i in range(n):
+    if a[i] >= num:
+        while a[i] >= num:
+            s.append(num)
+            num += 1
+            res += "+\n"
+        s.pop()
+        res += "-\n"
+    else:
+        n = s.pop()
+        if n > a[i]:
+            print("NO")
+            p = False
+            break
+        else:
+            res += "-\n"
+
+if p:
+    print(res)
