aaf14e3a91cb7f929ac85b9bbec005b5fa532ca3
죠 <68852637+stop0ho@users.noreply.github.com>
2023-09-04 16:56:45 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    let flag = true
+    if (s.length === 4 || s.length === 6) {
+        for (let i=0; i< s.length; i++){
+            if (Number(s[i]) != s[i]) {
+                flag = false
+            }
+        }
+    } else {
+        flag = false
+    }
+    return flag;
+}
