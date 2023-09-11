1d2798b229c8df14494995d13db1f667948fa6d6
Jyo <jyo.velop@gmail.com>
2023-09-11 16:07:08 +0900

    [Silver I] Title: 그림, Time: 356 ms, Memory: 46216 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,5 +1,4 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -10,23 +9,26 @@ area = 0 # 넓이
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-def dfs(i, j):
+def bfs(i, j):
   global temp
+  q = [(i, j)]
   graph[i][j] = 0
-  temp += 1
-
-  for k in range(4):
-    newX = dx[k] + j
-    newY = dy[k] + i
-    if 0 <= newX < m and 0 <= newY < n and graph[newY][newX]:
-      dfs(newY, newX)
+  while q:
+    ey, ex = q.pop()
+    for k in range(4):
+      newY = dy[k] + ey
+      newX = dx[k] + ex
+      if 0 <= newX < m and 0 <= newY < n and graph[newY][newX]:
+        q.append((newY, newX))
+        graph[newY][newX] = 0
+        temp += 1
 
 
 for i in range(n):
   for j in range(m):
     if graph[i][j]:
-      temp = 0
-      dfs(i, j)
+      temp = 1
+      bfs(i, j)
       if temp > area:
         area = temp
       cnt += 1
