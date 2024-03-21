6fb7094b21318bec9bc440450c544c9192ab86b0
Jyo <jyo.velop@gmail.com>
2024-03-21 21:06:14 +0900

    [Silver III] Title: 1로 만들기, Time: 888 ms, Memory: 87904 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,22 @@
-n = int(input())
-dp = [0] * (n+3)
+from collections import deque
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1 # 1을 빼는 연산
-    if i%2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    if i%3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+x = int(input())
 
-print(dp[n])
+dp = deque([x])
+result = -1
+flag = False
+
+while not flag:
+  for _ in range(len(dp)):
+    n = dp.popleft()
+    if n == 1:
+      flag = True
+      break
+    if n % 3 == 0:
+      dp.append(n//3)
+    if n % 2 == 0:
+      dp.append(n//2)
+    dp.append(n-1)
+  result += 1
+
+print(result)
