8b940e67fcd522c414b08ae0a4a17ac8d4dfe17d
Jyo <jyo.velop@gmail.com>
2024-03-28 10:09:10 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,10 +1,12 @@
 n = int(input())
-dp = [[0] * 10 for _ in range(n+1)]
+dp = [[0] * 10 for _ in range(101)]
 
-dp[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+for i in range(1, 10):
+  dp[1][i] = 1
+
 for i in range(2, n+1):
-  dp[i][0] = dp[i-1][1] # 0의 앞에는 1만 올 수 있음
-  dp[i][9] = dp[i-1][8] # 9의 앞에는 8만 올 수 있음
+  dp[i][0] = dp[i-1][1]
+  dp[i][9] = dp[i-1][8]
   for j in range(1, 9):
     dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
