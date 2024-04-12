d504b5c6effb3641ebcd03827b3521c0b4e30d2d
Jyo <jyo.velop@gmail.com>
2024-04-12 16:59:51 +0900

    [Gold IV] Title: 빙산, Time: 4392 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,67 +1,68 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
+def check(i, j, n, m):
+    return 0 <= i < n and 0 <= j < m
 
-# 동서남북 네 방향으로 붙어있는 0이 저장된 칸의 개수만큼 줄어듦
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+def melting(icebergs, n, m):
+    new_icebergs = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if icebergs[i][j] > 0:
+                count = 0
+                for k in range(4):
+                    ni = i + dx[k]
+                    nj = j + dy[k]
+                    if check(ni, nj, n, m) and icebergs[ni][nj] == 0:
+                        count += 1
+                new_icebergs[i][j] = max(icebergs[i][j] - count, 0)
+    return new_icebergs
 
-# 섬 개수
-def bfs(i, j):
-  q = deque([[i, j]])
-  visited[i][j] = True
+def bfs(icebergs, visited, start, n, m):
+    q = deque([start])
+    visited[start[0]][start[1]] = True
 
-  while q:
-    y, x = q.popleft()
-    for k in range(4):
-      ny = y + dy[k]
-      nx = x + dx[k]
-      if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx] and board[ny][nx]:
-        visited[ny][nx] = True
-        q.append([ny, nx])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if check(nx, ny, n, m) and icebergs[nx][ny] > 0 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
 
-# 무조건 한 덩어리의 빙산 주어진다 했으니까 최소 한 번은 돌아야 함.
-ans = 0
+def count_islands(icebergs, n, m):
+    islands = 0
+    visited = [[False] * m for _ in range(n)]
 
-while True:
-  # 1년 뒤 녹는 것
-  ans += 1
-  minus = deque()
-  for i in range(N):
-    for j in range(M):
-      if board[i][j] > 0:
-        cnt = 0
-        for k in range(4):
-          ny = dy[k] + i
-          nx = dx[k] + j
-          if 0 <= ny < N and 0 <= nx < M:
-            if board[ny][nx] == 0:
-              cnt += 1
-        minus.append([i, j, cnt])
-  
-  if len(minus) == 0:
-    print(0)
-    break
-  
-  while minus:
-    i, j, cnt = minus.pop()
-    board[i][j] -= cnt
-    # 각 칸에 저장된 높이는 0보다 더 줄어들지 않음
-    if board[i][j] < 0:
-      board[i][j] = 0
+    for i in range(n):
+        for j in range(m):
+            if icebergs[i][j] > 0 and not visited[i][j]:
+                bfs(icebergs, visited, (i, j), n, m)
+                islands += 1
 
-  island = 0
-  visited = [[False] * M for _ in range(N)]
-  for i in range(N):
-    for j in range(M):
-      if board[i][j] and not visited[i][j]:
-          if island == 0:
-            island += 1
-            bfs(i, j)
-          else:
-            print(ans)
-            exit()
+    return islands
+
+def main():
+    n, m = map(int, input().split())
+    icebergs = [list(map(int, input().split())) for _ in range(n)]
+    
+    years = 0
+    while True:
+        islands = count_islands(icebergs, n, m)
+        
+        if islands == 0:
+            print(0)
+            return
+        
+        if islands >= 2:
+            print(years)
+            return
+        
+        icebergs = melting(icebergs, n, m)
+        years += 1
+
+if __name__ == "__main__":
+    main()
