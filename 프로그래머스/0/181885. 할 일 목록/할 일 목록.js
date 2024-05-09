0643a5813be82823afd5b71c17e96d8c3da20293
Jyo <jyo.velop@gmail.com>
2024-05-09 21:32:43 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,3 @@
 function solution(todo_list, finished) {
-    let answer = []
-    todo_list.forEach((todo, index) => {
-        if (!finished[index]) {
-            answer.push(todo)
-        }
-    })
-    return answer
+    return todo_list.filter((todo, index) => finished[index]===false)
 }
