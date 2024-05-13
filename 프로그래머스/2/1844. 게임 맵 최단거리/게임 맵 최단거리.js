d0cd195b573aec33777fd056f4a8068885fc3080
Jyo <jyo.velop@gmail.com>
2024-05-13 21:28:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 28.10 ms, Memory: 40.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,33 @@
+function solution(maps) {
+    N = maps.length;
+    M = maps[0].length;
+    
+    let dist = Array.from(Array(N), () => new Array(M).fill(-1))
+
+    // 상 하 좌 우
+    let dy = [-1, 1, 0, 0]
+    let dx = [0, 0, -1, 1]
+    
+    let q = [[0, 0]]
+    maps[0][0] = 0
+    dist[0][0] = 1
+    
+    while (q.length !== 0) {
+        let [y, x] = q.shift()
+        
+        for (let k = 0; k < 4; k++){
+            let newY = dy[k] + y
+            let newX = dx[k] + x
+            
+            if (0 <= newY && newY < N && 0 <= newX && newX < M && maps[newY][newX]) {
+                maps[newY][newX] = 0 // 방문처리
+                dist[newY][newX] = dist[y][x] + 1
+                q.push([newY, newX])
+            }
+        }
+    }
+    
+    console.log(dist)
+    
+    return dist[maps.length - 1][maps[0].length - 1];
+}
