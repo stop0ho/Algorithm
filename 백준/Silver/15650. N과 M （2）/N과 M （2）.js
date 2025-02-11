cdf90adda84d2601b964c8c8b9f8058f38bd3d46
Jiho Jeong <jyo.velop@gmail.com>
2025-02-11 14:00:15 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 9480 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map((v) => Number(v));
+const answer = Array(10).fill(0);
+const visited = Array(10).fill(false);
+
+function dfs(k, c) {
+  if (k == M) {
+    console.log(answer.filter((_, i) => i < M).join(" "));
+    return;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (!visited[i] && i > c) {
+      visited[i] = true;
+      answer[k] = i;
+      dfs(k + 1, i);
+      visited[i] = false;
+    }
+  }
+}
+
+dfs(0, 0);
